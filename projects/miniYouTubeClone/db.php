5760481fdbf66c4ea8cb1add2bdce2748ba988a5
tschams <tschams2@gmail.com>
2018-08-30 23:52:57 -0400

    a

--- projects/miniYouTubeClone/db.php
+++ projects/miniYouTubeClone/db.php
@@ -1,7 +1,7 @@
 <?php
     $cs = "mysql:host=167.99.224.176;dbname=forge";
     $user = "forge";
-    $password = getenv('DB_PASSWORD');
+    $password = $_ENV('DB_PASSWORD');
 
     try {
         $options = [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION];
