1531ed00af7bdb32b0c31066bfbeb91737b3cef6
tschams <tschams2@gmail.com>
2018-09-12 14:30:26 -0400

    q

--- projects/PHPCRUD/realestate/utils/db.php
+++ projects/PHPCRUD/realestate/utils/db.php
@@ -1,6 +1,6 @@
 <?php
     try {
-        $db = new PDO("mysql:host=167.99.224.176;dbname=forge", "forge", getenv('DB_PASSWORD'),
+        $db = new PDO("mysql:host=167.99.224.176;dbname=forge", "forge", "getenv(DB_PASSWORD)",
             [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
     } catch (PDOException $e) {
         $error = "Something went wrong " . $e->getMessage();
